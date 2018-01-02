2c21cfec8632127d8c9d41a49a1c8d02842151d5
wolyshaw <wolyshaw@users.noreply.github.com>
2018-01-02 21:04:36 +0800

    css-loader bug

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import Pages from 'components/Pages'
+import './static/index.less'
 
 export const Application = props => {
   return (
