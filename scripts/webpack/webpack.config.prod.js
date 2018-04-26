8a9883760259fbcfc582005a57315b2548f0aa41
wolyshaw <wolyshaw@users.noreply.github.com>
2018-04-26 10:17:42 +0800

    npm run fix

--- scripts/webpack/webpack.config.prod.js
+++ scripts/webpack/webpack.config.prod.js
@@ -2,9 +2,8 @@ const merge = require('webpack-merge')
 const baseConfig = require('./webpack.config.base')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const lessConfig = require('../../less.config')
 
-module.exports = [merge(baseConfig, {
+module.exports = merge(baseConfig, {
   name: 'web',
   output: {
     filename: '[name]-[chunkhash:5].js',
@@ -19,70 +18,4 @@ module.exports = [merge(baseConfig, {
       new OptimizeCSSAssetsPlugin({})
     ]
   }
-}),
-merge(baseConfig, {
-  entry: './src/render',
-  output: {
-    filename: 'render.js',
-    publicPath: '/dist/',
-    libraryTarget: 'commonjs2'
-  },
-  target: 'node',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [['env', {'targets': {browsers: ['last 2 versions', 'ie >= 9']}, modules: false}], 'react'],
-              plugins: [
-                ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
-                'transform-runtime',
-                'transform-decorators',
-                'transform-class-properties',
-                'transform-object-rest-spread'
-              ]
-            }
-          },
-          {
-            loader: 'eslint-loader',
-            options: {fix: true}
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-        include: /node_modules/,
-        use: ['css-loader/locals?minimize']
-      },
-      {
-        test: /\.less$/,
-        include: /node_modules/,
-        use: [
-          'css-loader/locals?minimize',
-          {
-            loader: 'less-loader',
-            options: {
-              javascriptEnabled: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(css|less)$/,
-        exclude: /node_modules/,
-        use: [
-          'css-loader/locals?modules&localIdentName=[local]--[hash:base64:5]',
-          {
-            loader: 'less-loader',
-            options: lessConfig
-          }
-        ]
-      }
-    ]
-  },
-  plugins: []
-})]
+})
