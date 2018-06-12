cabe32b0b1169da0827779b70949361ecc912937
肖卧龙 <wolyshaw@users.noreply.github.com>
2018-06-12 15:40:55 +0800

    home page

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -12,12 +12,29 @@ export default class Home extends PureComponent {
     location: PropTypes.object,
   }
 
+  submit = e => {
+    e.preventDefault()
+    const formData = new FormData()
+    const { files } = e.target.querySelector('.file')
+    Array.from(files).map(file => formData.append('filekey', file))
+
+    fetch('/api/v1/upload', {
+      body: formData,
+      method: 'POST'
+    })
+      .then(r => console.log(r))
+  }
+
   render() {
     return (
       <div className={ styles.home }>
         you react project bulid success!
         <span onClick={() => dispatch({type: 'SET_USER_INFO', data: false})}>重新渲染</span>
         <Link to='/test'>go Test</Link>
+        <form onSubmit={ this.submit }>
+          <input className="file" type="file" multiple/>
+          <input type="submit" value="submit"/>
+        </form>
       </div>
     )
   }
