ef0d3d74bc35107ceb63e698d34564ca3af6f136
wolyshaw <wolyshaw@users.noreply.github.com>
2019-12-09 22:23:00 +0800

    add code split

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { PureComponent } from 'react'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+import loadable from '@loadable/component'
+
+const Home = loadable(() => import('./pages/home'))
 
 export default class App extends PureComponent {
   render() {
     return (
-      <div>app</div>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+      </Router>
     )
   }
 }
